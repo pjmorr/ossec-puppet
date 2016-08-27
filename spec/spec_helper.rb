7a9aca17d6f08b281bfcb818d136510df61094bb
Philip Morrison <phil@uberops.com>
2016-08-27 16:29:40 -0500

    pjmorr/ossec-puppet#1 fixing specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,3 +23,53 @@ end
 # class Object
 #    alias :must :should
 # end
+
+unless $executing_puppet
+  begin
+    gem 'simplecov'
+    require 'simplecov'
+    formatters = []
+    formatters << SimpleCov::Formatter::HTMLFormatter
+
+    begin
+      gem 'coveralls'
+      require 'coveralls'
+      formatters << Coveralls::SimpleCov::Formatter if ENV['TRAVIS']
+    rescue Gem::LoadError
+      # do nothing
+    end
+
+    begin
+      gem 'codeclimate-test-reporter'
+      require 'codeclimate-test-reporter'
+      formatters << CodeClimate::TestReporter::Formatter if ENV['TRAVIS'] && ENV['CODECLIMATE_REPO_TOKEN']
+    rescue Gem::LoadError
+      # do nothing
+    end
+    SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[*formatters]
+    SimpleCov.start do
+      add_filter '/spec/'
+      add_filter './vendor/'
+      add_filter './.vendor/'
+      add_filter '/.vendor/'
+      add_filter '/vendor/'
+      add_filter '/gems/'
+      minimum_coverage 76
+      refuse_coverage_drop
+    end
+  rescue Gem::LoadError
+    # do nothing
+  end
+end
+
+begin
+  gem 'pry'
+  require 'pry'
+rescue Gem::LoadError
+  # do nothing
+end
+
+module Testing
+  module Mock end
+end
+at_exit { RSpec::Puppet::Coverage.report! }
